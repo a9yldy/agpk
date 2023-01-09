7c41c059fa5402208a932be2fcf9500ab98fd02e
aqyldy <115893323+aqyldyman@users.noreply.github.com>
2023-01-09 23:46:09 +0600

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
